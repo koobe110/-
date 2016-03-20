828001cf03b3e6e24f410b9a0d54356a15d9895f
koobe110 <xulongxi@foxmail.com>
2016-03-20 09:15:31 +0800

    Added files via upload

---
+++ Books.java
@@ -0,0 +1,23 @@
+package com.imooc;
+public class Books {
+	public int num;
+	public String name;
+	public Books(int num, String name) {
+		// TODO Auto-generated constructor stub
+		this.num = num;
+		this.name = name;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
